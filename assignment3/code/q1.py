b20291b61ff4aff3b8ee7f43e39f7f017a860abe
sida <sliu1@uvm.edu>
2021-03-05 09:02:09 -0500

    save

--- assignment3/code/q1.py
+++ assignment3/code/q1.py
@@ -9,8 +9,8 @@ df = pd.read_csv("mlbootcamp5_train.csv", nrows = 4000)
 df["height_n"] = df["height"]/df["height"].mean()
 df["weight_n"] = df["weight"]/df["weight"].mean()
 
-x_cols = "height_n,cholesterol,gluc,smoke,alco,active,cardio".split(",")
-y_col = "weight_n"
+x_cols = "height_n,weight_n,cholesterol,gluc,alco,active,cardio".split(",")
+y_col = "smoke"
 num_cols = len(x_cols)
 
 x = df[x_cols].to_numpy()
@@ -24,14 +24,28 @@ y_test = y[num_train:]
 
 # weights: the thetas
 w = np.random.random([num_cols,1])
+def model(x):
+    """A discrimitive model"""
+    return np.matmul(x, w).flatten()
+
+# Test
+def test_model():
+    """test the model with existing w"""
+    y_hat = model(x_test)
+    l_mean_after = ((y_test-y_hat)**2).mean()
+    print(f"Test loss: {l_mean_after}")
+
+    y_hat=(y_hat>0.5).astype(np.int)
+    accuracy = (y_test==y_hat).sum() / len(y_hat)
+    print(f"Accuracy {accuracy}")
+
+    print("")
 
 # Test before training
-y_hat = np.matmul(x_test, w)
-l_mean_before = ((y_test-y_hat)**2).mean()
-print(f"Test loss before training: {l_mean_before}")
-
+print("Before training:")
+test_model()
 # Train
-gamma = 1e-6
+gamma = 1e-5
 num_epochs = int(1e4)
 batch_size = 100
 for i in range(num_epochs):
@@ -39,7 +53,7 @@ for i in range(num_epochs):
         x_batch = x_train[j:j+batch_size, :]
         y_batch = y_train[j:j+batch_size]
 
-        y_hat = np.matmul(x_batch,w).flatten()
+        y_hat = model(x_batch)
         diff = y_batch - y_hat
 
         gradient = -2 * np.matmul(x_batch.T, np.expand_dims(diff,-1))
@@ -49,7 +63,6 @@ for i in range(num_epochs):
         print(f"Train loss: {l_sum}")
 
 # Test after training
-y_hat = np.matmul(x_test, w)
-l_mean_after = ((y_test-y_hat)**2).mean()
-print(f"Test loss after training: {l_mean_after}")
-print(f"Improved {l_mean_before-l_mean_after}")
+
+print("After training:")
+test_model()
